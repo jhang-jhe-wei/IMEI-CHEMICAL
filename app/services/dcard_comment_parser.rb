21a0262a709d7ca92f4b7b2ad1141a73ed1887f9
wells <jhang0912407249@gmail.com>
2022-01-08 03:15:32 +0800

    Add dcard comment parser

--- app/services/dcard_comment_parser.rb
+++ app/services/dcard_comment_parser.rb
@@ -3,6 +3,8 @@ class DcardCommentParser
   Capybara.default_driver = :selenium_chrome
   # Capybara.default_driver = :selenium_chrome_headless
 
+  CACHE_FILE_PATH = "#{Rails.root}/app/services/dcard_post_urls.json"
+
   def initialize(search_text)
     @search_text = search_text
     @b = Capybara.current_session
@@ -10,25 +12,23 @@ class DcardCommentParser
 
   def perform
     post_urls.each do |post_url|
-      comments = get_post_comments(post_url)
-      comments.each do |comment_id, data|
-        comment_data = {user_name: data[0], context: data[1], posted_time: data[4], source_url: post_url, source_type: "Dcard"}
-        puts(comment_data)
-        Comment.create!(comment_data)
+      puts "爬取#{post_url}"
+      ActiveRecord::Base.transaction do
+        load_post_comments(post_url)
       end
     end
+    File.delete(CACHE_FILE_PATH) if File.exist?(CACHE_FILE_PATH)
     Capybara.current_session.driver.quit
   end
 
-  private
-
-  def get_post_comments(post_url)
+  def load_post_comments(post_url)
     @b.visit(post_url)
-    return {} if @b.all("#comment").empty?
-    comment_data = {}
+    return if @b.all("#comment").empty?
+    data_keys = []
     number_of_comments = @b.all("#comment>div>div").first&.text&.scan(/\d/)&.join&.to_i
     @b.scroll_to(@b.find("#comment"))
-    until comment_data.size >= number_of_comments
+    until data_keys.size >= number_of_comments
+      puts "取得留言中，目前進度#{(data_keys.size.to_f/number_of_comments.to_f) * 100} %"
       sleep(1)
       @b.execute_script <<~Javascript
       document.querySelectorAll("#comment div[data-key^='subCommentToggle-'] button").forEach((btn)=>{
@@ -37,27 +37,54 @@ class DcardCommentParser
         }
       })
       Javascript
-      sleep(1)
-      @b.all("#comment div[data-key^='comment-']").map do |div|
-        unless comment_data[div["data-key"]]
-          comment_data[div["data-key"]] = div.all("span").map(&:text)
+      sleep(2)
+      @b.all("#comment div[data-key^='comment-']").each do |div|
+        unless data_keys.include? div["data-key"]
+          data_keys << div["data-key"]
+          data = div.all("span").map(&:text)
+          byebug
+          data[1..-4].each do |comment|
+            comment_data = {user_name: data[0], context: comment, posted_at: data[-1], source_url: post_url, source_type: "Dcard"}
+            puts "寫入資料#{comment_data.to_s}"
+            Comment.create!(comment_data)
+          end
         end
       end
-      @b.scroll_to(@b.all("#comment div[data-key^='comment-']").last)
+      # @b.scroll_to(@b.all("#comment div[data-key^='comment-']").last)
+      @b.execute_script "window.scrollBy(0,1000)"
+        end
+    end
+
+    def post_urls
+      if File.exist? CACHE_FILE_PATH
+        JSON.parse(File.open(CACHE_FILE_PATH).read)
+      else
+        search_post_urls(@search_text)
       end
-      comment_data
+    end
+
+    def search_post_urls(search_text)
+      @b.visit("https://www.dcard.tw/search/posts?query=#{search_text}&field=title")
+        post_urls = []
+      until @b.has_content?("沒有更多文章囉！")
+        @b.scroll_to(@b.all('div[data-key^="post-"] a').last)
+        sleep(1)
+        @b.all('div[data-key^="post-"] a').each do |a|
+          post_url = a[:href]
+          puts "取得文章連結#{post_url}"
+          if post_urls.include?(post_url)
+            puts "此連結已被存入"
+          else
+            post_urls << post_url
+          end
+        end
+      end
+      File.open(CACHE_FILE_PATH, "a+") do |f|
+        f.write(post_urls.to_json)
+      end
+      post_urls
+    end
   end
 
-  def post_urls
-    unless @post_urls
-      @b.visit("https://www.dcard.tw/search/posts?query=#{@search_text}&field=title")
-        @post_urls = []
-      until @b.has_content?("沒有更多文章囉！")
-        @post_urls << @b.all('div[data-key^="post-"] a').map {|a| a[:href]}
-        @b.scroll_to(@b.all('div[data-key^="post-"] a').last)
-      end
-      @post_urls.flatten!.uniq!
-    end
-    @post_urls
-  end
-end
+
+  # DcardCommentParser.new("麥片").perform
