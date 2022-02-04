9fca249d6ba90b952aa5ce4f17cd36d3037b7ba4
wells <jhang0912407249@gmail.com>
2022-02-04 20:07:44 +0800

    Change deploy user to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, "git@github.com:jhang-jhe-wei/kugifoods-rails.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/wells/kugifoods-rails"
+set :deploy_to, "/home/deploy/kugifoods-rails"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -22,7 +22,7 @@ set :deploy_to, "/home/wells/kugifoods-rails"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml","config/secrets.yml",".env"
+append :linked_files, "config/database.yml","config/secrets.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-server "kugifoods-rails.wells.tw", user: "wells", roles: %w{app db web}, my_property: :my_value
+server "kugifoods-rails.wells.tw", user: "deploy", roles: %w{app db web}, my_property: :my_value
 
 
 
