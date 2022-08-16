ff614a3d3b1d837108c1269376254f9ff962818e
wells <jhang0912407249@gmail.com>
2022-08-17 04:19:58 +0800

    chore: Update cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,14 @@ sh "ssh-add ~/.ssh/wells-ssh-key"
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "kugifoods-rails"
-set :repo_url, "git@github.com:jhang-jhe-wei/kugifoods-rails.git"
+set :application, "imei-chemical"
+set :repo_url, "git@github.com:jhang-jhe-wei/IMEI-CHEMICAL.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/kugifoods-rails"
+set :deploy_to, "/home/deploy/imei-chemical"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
