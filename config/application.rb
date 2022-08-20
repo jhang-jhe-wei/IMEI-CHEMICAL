ef06b7b8d36f0ca7a8d0558baf54836b666c98fe
wells <jhang0912407249@gmail.com>
2022-08-20 10:10:47 +0800

    style: Change application title

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Kugifood
+module ImeiChemical
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
